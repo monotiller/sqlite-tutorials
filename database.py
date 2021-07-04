1b3ad68706a078c1ea008557b7d71d372a53f924
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-04 16:09:32 +0100

    Complete Update Records
    https://www.youtube.com/watch?v=byHcYRpMgI4&t=3077s

--- database.py
+++ database.py
@@ -6,8 +6,15 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor
 c = conn.cursor()
 
+# Update records
+c.execute("""UPDATE customers SET first_name = 'Bob'
+            WHERE last_name = 'Smith'
+""")
+
+conn.commit()
+
 # Query the database
-c.execute("SELECT * FROM customers WHERE last_name = 'Smith'") # WHERE allows you to filter results, you can use expressions like you can in for loops and the likes, i.e. age >= 21. You can also use LIKE and wildcards (defined by %) to find things that are similar i.e. %example.com to search email addresses that end in that domain.
+c.execute("SELECT * FROM customers")
 
 items = c.fetchall()
 print(items)
