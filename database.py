585225af1b34374e33bdcc048c308ea99db7a41f
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-04 16:14:42 +0100

    Completed Order Results
    https://www.youtube.com/watch?v=byHcYRpMgI4&t=3507s

--- database.py
+++ database.py
@@ -6,13 +6,8 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor
 c = conn.cursor()
 
-# Delete Records
-c.execute("DELETE from customers WHERE rowid = 6")
-
-conn.commit()
-
-# Query the database
-c.execute("SELECT * FROM customers")
+# Query the database - ORDER BY
+c.execute("SELECT * FROM customers ORDER BY last_name")
 
 items = c.fetchall()
 print(items)
