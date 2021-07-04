7104ad1a6ac9b726e1da9c7d19f1d7bc00c4a5d1
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-04 16:18:21 +0100

    Completed And/Or
    https://www.youtube.com/watch?v=byHcYRpMgI4&t=3697s

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ conn = sqlite3.connect('customer.db')
 c = conn.cursor()
 
 # Query the database - ORDER BY
-c.execute("SELECT * FROM customers ORDER BY last_name")
+c.execute("SELECT * FROM customers WHERE last_name LIKE 'Br%' OR rowid = 3") # So we can use this where we vaguely remember someone's surname but we might remember the rowid
 
 items = c.fetchall()
 print(items)
