8c6a234cf53e1cc6d13fc18eae26ac851a5117ef
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-04 15:27:27 +0100

    Completed Insert Many Records Into Table
    https://www.youtube.com/watch?v=byHcYRpMgI4&t=1885s

--- database.py
+++ database.py
@@ -6,8 +6,15 @@ conn = sqlite3.connect('customer.db')
 # Create a cursor
 c = conn.cursor()
 
+# Create a python list with multiple entries
+many_customers = [
+    ('Wes,' 'Brown', 'wes@example.com'),
+    ('Joe', 'Bloggs', 'joe@example.com'),
+    ('John', 'Smith', 'john@example.com'),
+]
+
 # Create a table
-c.execute ("INSERT INTO customers VALUES ('Leah', 'Smith', 'leah@example.com')")
+c.executemany("INSERT INTO customers VALUES (?, ?, ?)", many_customers) # ? is used as the placeholder in SQLite
 
 # Commit our command
 conn.commit()
