bd4ecdfdab86235c20a007dce054697db435ae0a
jrmswell <18310129+monotiller@users.noreply.github.com>
2021-07-04 16:50:15 +0100

    Completed Our App - Show All Function
    https://www.youtube.com/watch?v=byHcYRpMgI4&t=4154s

--- database.py
+++ database.py
@@ -1,24 +1,23 @@
 import sqlite3
 
-# Connect to database
-conn = sqlite3.connect('customer.db')
+# Query the DB and return all records
+def show_all():
+    # Connect to database
+    conn = sqlite3.connect('customer.db')
 
-# Create a cursor
-c = conn.cursor()
+    # Create a cursor
+    c = conn.cursor()
 
-# Drop table
-c.execute("DROP TABLE customers")
-conn.commit()
+    # Query the database - ORDER BY
+    c.execute("SELECT rowid, * FROM customers")
+    
+    items = c.fetchall()
 
-# Query the database - ORDER BY
-c.execute("SELECT rowid, * FROM customers")
+    for item in items:
+        print(item)
 
-items = c.fetchall()
-for item in items:
-    print(item)
+    # Commit our command
+    conn.commit()
 
-# Commit our command
-conn.commit()
-
-# Close our connection
-conn.close
+    # Close our connection
+    conn.close
